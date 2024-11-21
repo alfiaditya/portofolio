195ac0898f89cc6395493ab562c1ff08173b5097
alfiaditya <alfiaditya12@gmail.com>
2024-11-21 19:35:16 +0700

    skill

--- src/components/SkillSection.vue
+++ src/components/SkillSection.vue
@@ -1,81 +1,69 @@
 <template>
-    <section class="relative w-full mt-20" id="skills" data-aos="zoom-in-up">
-      <div class="absolute top-0 inset-x-0 flex items-start">
-        <div class="h-24 w-2/3 bg-gradient-to-br from-[#570cac] blur-2xl invisible opacity-40"></div>
-        <div class="h-20 w-3/5 bg-gradient-to-br from-[#670ccf] opacity-40 blur-2xl"></div>
-      </div>
-      <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-screen-lg lg:max-w-screen-xl mx-auto relative">
-        <div class="grid lg:grid-cols-2 gap-10 xl:gap-14 relative pt-24 lg:max-w-none max-w-2xl md:max-w-3xl mx-auto">
-          <div class="lg:h-full md:flex">
-            <div class="flex w-full h-96 min-h-[24rem] lg:min-h-[none] lg:w-full lg:h-full items-center relative">
-              <div class="absolute z-0 top-1/2 -translate-y-1/2 w-5/6 right-0 h-[calc(80%-20px)] bg-gradient-to-tr opacity-25 from-[570cac] to-primary blur-2xl"></div>
-              <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-8 rounded-lg shadow-lg border border-primary bg-[#111a3e]">
-                <h2 class="text-2xl font-semibold text-white mb-4 col-span-2">My Skills</h2>  
-                <div class="mb-4">
-                  <h3 class="text-xl font-semibold text-white mb-2">PHP</h3>
-                  <div class="w-full bg-gray-200 rounded-full h-2.5">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style="width: 80%"></div>
-                  </div>
-                  <p class="text-white mt-2">4 tahun penggunaan</p>
-                </div>
-                <div class="mb-4">
-                  <h3 class="text-xl font-semibold text-white mb-2">Laravel</h3>
-                  <div class="w-full bg-gray-200 rounded-full h-2.5">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style="width: 20%"></div>
-                  </div>
-                  <p class="text-white mt-2">1 tahun penggunaan</p>
-                </div>
-                <div class="mb-4">
-                  <h3 class="text-xl font-semibold text-white mb-2">React</h3>
-                  <div class="w-full bg-gray-200 rounded-full h-2.5">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style="width: 20%"></div>
-                  </div>
-                  <p class="text-white mt-2">1 tahun penggunaan</p>
-                </div>
-                <div class="mb-4">
-                  <h3 class="text-xl font-semibold text-white mb-2">Node.js</h3>
-                  <div class="w-full bg-gray-200 rounded-full h-2.5">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style="width: 20%"></div>
-                  </div>
-                  <p class="text-white mt-2">1 tahun penggunaan</p>
-                </div>
-                <div class="mb-4">
-                  <h3 class="text-xl font-semibold text-white mb-2">Express.js</h3>
-                  <div class="w-full bg-gray-200 rounded-full h-2.5">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style="width: 20%"></div>
-                  </div>
-                  <p class="text-white mt-2">1 tahun penggunaan</p>
-                </div>
-                <div class="mb-4">
-                  <h3 class="text-xl font-semibold text-white mb-2">Hyperledger Fabric</h3>
-                  <div class="w-full bg-gray-200 rounded-full h-2.5">
-                    <div class="bg-blue-600 h-2.5 rounded-full" style="width: 20%"></div>
-                  </div>
-                  <p class="text-white mt-2">1 tahun penggunaan</p>
-                </div>
+  <section class="relative w-full mt-20" id="skills" data-aos="zoom-in-up">
+    <div class="absolute top-0 inset-x-0 flex items-start">
+      <div class="h-24 w-2/3 bg-gradient-to-br from-[#570cac] blur-2xl invisible opacity-40"></div>
+      <div class="h-20 w-3/5 bg-gradient-to-br from-[#670ccf] opacity-10 blur-2xl"></div>
+    </div>
+
+    <div class="w-full px-5 sm:px-8 md:px-12 lg:px-8 max-w-screen-lg lg:max-w-screen-xl mx-auto relative">
+      <div class="grid lg:grid-cols-2 gap-10 xl:gap-14 relative pt-24 lg:max-w-none max-w-2xl md:max-w-3xl mx-auto">
+        <div class="lg:h-full md:flex">
+          <div class="flex w-full h-96 min-h-[24rem] lg:min-h-[none] lg:w-full lg:h-full items-center relative">
+            <div class="absolute z-0 top-1/2 -translate-y-1/2 w-5/6 right-0 h-[calc(80%-20px)]  bg-gradient-to-br from-[#670ccf] opacity-10 blur-2xl"></div>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-8 rounded-lg shadow-lg border border-primary bg-[#111a3e]">
+              <h2 class="text-2xl font-semibold text-white mb-4 col-span-2">My Skills</h2>
+
+              <div class="mb-4">
+                <h3 class="text-xl font-semibold text-white mb-2">PHP</h3>
               </div>
-            </div>
-          </div>
-          <div class="lg:py-6">
-            <div class="text-center lg:text-left">
-              <h1 class="pt-4 text-white font-bold text-4xl md:text-5xl lg:text-6xl">
-                Work <span class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-pink-500">Experience</span>
-              </h1>
-            </div>
-            <div class="text-gray-300 pt-8 text-left mx-auto">
-              <div class="mb-6">
-                <h3 class="text-xl font-semibold text-white mb-2">IT Support</h3>
-                <p class="text-lg text-gray-400">PT Askrindo - Bandung, Jawa Barat</p>
-                <p class="text-gray-400">Januari 2020 - Maret 2020</p>
+
+              <div class="mb-4">
+                <h3 class="text-xl font-semibold text-white mb-2">Laravel</h3>
               </div>
-              <div class="mb-6">
-                <h3 class="text-xl font-semibold text-white mb-2">Frontend</h3>
-                <p class="text-lg text-gray-400">Palang Merah Indonesia - Bandung, Jawa Barat</p>
-                <p class="text-gray-400">Juli 2023 - Agustus 2023</p>
+
+              <div class="mb-4">
+                <h3 class="text-xl font-semibold text-white mb-2">React</h3>
               </div>
+
+              <div class="mb-4">
+                <h3 class="text-xl font-semibold text-white mb-2">Node.js</h3>
+              </div>
+
+              <div class="mb-4">
+                <h3 class="text-xl font-semibold text-white mb-2">Express.js</h3>
+              </div>
+
+              <div class="mb-4">
+                <h3 class="text-xl font-semibold text-white mb-2">Hyperledger Fabric</h3>
+              </div>
+
             </div>
           </div>
         </div>
+
+        <div class="lg:py-6">
+          <div class="text-center lg:text-left">
+            <h1 class="pt-4 text-white font-bold text-4xl md:text-5xl lg:text-6xl">
+              Work <span class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-pink-500">Experience</span>
+            </h1>
+          </div>
+          <div class="text-gray-300 pt-8 text-left mx-auto">
+            <div class="mb-6">
+              <h3 class="text-xl font-semibold text-white mb-2">IT Support</h3>
+              <p class="text-lg text-gray-400">PT Askrindo - Bandung, Jawa Barat</p>
+              <p class="text-gray-400">Januari 2020 - Maret 2020</p>
+            </div>
+
+            <div class="mb-6">
+              <h3 class="text-xl font-semibold text-white mb-2">Frontend</h3>
+              <p class="text-lg text-gray-400">Palang Merah Indonesia - Bandung, Jawa Barat</p>
+              <p class="text-gray-400">Juli 2023 - Agustus 2023</p>
+            </div>
+          </div>
+        </div>
+
       </div>
-    </section>
-  </template>
+    </div>
+
+  </section>
+</template>
